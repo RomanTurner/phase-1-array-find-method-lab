2370cf59380ed62cfb082f689d8684cf34f0a23c
Roman Turner <rn.turner@live.com>
2021-02-23 12:55:43 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+function resultCheck(record) {
+  return record.find((obj) => obj.result === 'W');
+}
+
+function superbowlWin(record) {
+    let winObj = resultCheck(record)
+    if (winObj != undefined) {
+        return winObj.year;
+    } 
+    else {
+        return undefined;
+    }
+}
